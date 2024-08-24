56cb6acc67cb2e918e5074b97278b86f37185bb0
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-08-24 15:12:52 +0200

    fix: 154 sanity check for genesis block

--- state/block.go
+++ state/block.go
@@ -1,6 +1,7 @@
 package state
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -12,6 +13,10 @@ type Block struct {
 	BlockHash   common.Hash
 	ParentHash  common.Hash
 	ReceivedAt  time.Time
+}
+
+func (b *Block) String() string {
+	return fmt.Sprintf("BlockNumber: %d, BlockHash: %s, ParentHash: %s, ReceivedAt: %s", b.BlockNumber, b.BlockHash, b.ParentHash, b.ReceivedAt)
 }
 
 // NewBlock creates a block with the given data.
